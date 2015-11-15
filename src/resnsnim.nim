06aa467988f82cbd6af685b61d5c718c3a156167
Case Henry Dorsett <wintermute347@gmail.com>
2015-11-15 13:54:43 +0200

    Bugfix and delete debug info

--- src/resnsnim.nim
+++ src/resnsnim.nim
@@ -25,34 +25,45 @@ proc toString*(rnsnumb: RNSnumb): string =
 proc RNSadd*(rnsnumb1: RNSnumb, rnsnumb2: RNSnumb, basis: Modules): RNSnumb =
   if (len(rnsnumb1) == len(rnsnumb2)):
     for i in low(basis)..high(basis):
-      let summa: BigInt = rnsnumb1[i] + rnsnumb1[i]
+      let summa: BigInt = rnsnumb1[i] + rnsnumb2[i]
       let basis: BigInt = initBigInt(basis[i])
-
-      echo toString(summa)
 
       if summa < basis: 
         result[i] = summa
       else: 
         result[i] = summa mod basis
 
-      #echo toString(result[i])
-
 proc RNSmult*(rnsnumb1: RNSnumb, rnsnumb2: RNSnumb, basis: Modules): RNSnumb =
   for i in low(basis)..high(basis):
-    result[i] = ((rnsnumb1[i] * rnsnumb1[i]) mod initBigInt(basis[i]))
+    let multp: BigInt = rnsnumb1[i] * rnsnumb2[i]
+    let basis: BigInt = initBigInt(basis[i])
+
+    if multp < basis: 
+      result[i] = multp
+    else: 
+      result[i] = multp mod basis
 
 when isMainModule:
   var
     test0: BigInt  = initBigInt(25)
+
     test1: BigInt  = initBigInt(9)
     test2: BigInt  = initBigInt(14)
 
-    modls: Modules = [2, 3, 5]
+    test3: BigInt  = initBigInt(4)
+    test4: BigInt  = initBigInt(5)
 
-    numb1: RNSnumb = toRNS(test1, modls)
-    numb2: RNSnumb = toRNS(test2, modls)
+    basis1: Modules = [2, 3, 5]
+    basis2: Modules = [2, 3, 5]
 
-  echo toString(RNSadd(numb1, numb2, modls))
+    numb1: RNSnumb = toRNS(test1, basis1)
+    numb2: RNSnumb = toRNS(test2, basis1)
+
+    numb3: RNSnumb = toRNS(test3, basis2)
+    numb4: RNSnumb = toRNS(test4, basis2)
+
+  echo toString(RNSadd(numb1, numb2, basis1))
+  echo toString(RNSmult(numb3, numb4, basis2))
 
 
 
